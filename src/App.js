354f13f47e2d34cdb0bd685b59c46fade7e645ff
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-10 15:25:45 +0300

    yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react'
 
-function App() {
+export default function App() {
+  const [Data, setData] = useState("")
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => response.json())
+    .then(data => setData(data))
+  },[])
+  console.log(Data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {Data.map((item, i)=>{
+        
+      })}
+    </>
   );
 }
-
-export default App;
