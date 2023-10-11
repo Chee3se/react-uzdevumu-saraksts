fd2c10191fe712310e521a6fb47c992582e11e9f
Chee3se <emilspete999@gmail.com>
2023-10-11 22:35:01 +0300

    :o, wowo!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Task from "./Task"
-import {useState, useEffect} from 'react'
 
 export default function App() {
   const [Data, setData] = useState("")
+  const [Shown, setShown] = useState(10)
   useEffect(()=>{
+    //Fetching data
     fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
-    .then(data => setData(data))
-  },[])
+    .then((data) => {
+      console.log(data)
+      //Checking localstorage
+
+      if (typeof localStorage['data'] !==  'undefined') {
+        //is defined
+        data = JSON.parse(localStorage.getItem('data'))
+      } else {
+        //is NOT defined
+        localStorage.setItem('data', JSON.stringify(data))
+      }
+      //Final
+      setData(data)
+  })},[])
+  function handleClick(event) {
+    setShown(Shown !== 200 ? Shown + 10 : Shown)
+  }
   return (
     <>
-      <h1>yes</h1>
       {Object.entries(Data).map((item, i)=>{
-        return <Task key={i} info={item}></Task>
+        return i < Shown ? <Task key={i} info={item}></Task> : null
       })}
+      <button className='controller' onClick={handleClick}>show more</button>
     </>
   );
 }
--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,19 @@
+import React, {useState} from 'react'; 
+
 export default function Task(params) {
+  const [Completed, setCompleted] = useState(params.info[1].completed)
+  function handleChange(event) {
+    setCompleted(!Completed)
+    const Data = JSON.parse(localStorage.getItem('data'))
+    Data[event.target.value].completed = !Completed
+    console.log(Data[event.target.value])
+    localStorage.setItem('data', JSON.stringify(Data))
+  }
     return (
       <div className="task">
-        <input type="checkbox" checked={params.info[1].completed} /> 
+        <img src="person.svg" alt="person"/>
+        <p>{params.info[1].userId}</p>
+        <input type="checkbox" name="completed" value={(params.info[1].id)-1} checked={Completed} onChange={handleChange}/> 
         <label></label>
         <p>{params.info[1].title}</p>
       </div>
